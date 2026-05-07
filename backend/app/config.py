c0c2eae72a31bb1ba419b66a8e6ed34ae601c1b5
Sheersh <sheershsinha30@gmail.com>
2026-05-07 14:57:47 +0530

    Modified files and dubugged the issue of all microservice

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,4 +1,5 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
 class Settings(BaseSettings):
     POSTGRES_URL: str
     MONGO_URL: str
@@ -9,6 +10,6 @@ class Settings(BaseSettings):
     DEBOUNCE_WINDOW_SECONDS: int = 10
     DEBOUNCE_THRESHOLD: int = 100
 
-    class Config:
-        env_file = ".env"
+    model_config = SettingsConfigDict(env_file=".env")
+
 settings = Settings()
